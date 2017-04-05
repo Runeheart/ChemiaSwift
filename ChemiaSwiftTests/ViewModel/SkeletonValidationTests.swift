17b0d90e355c4aaadfbbc8f1a27a4301887c02c7
Jonah Austin <jonahaustin@Jonahs-MacBook.local>
2017-04-04 22:29:55 -0400

    Wanted to add unit tests to cover the simplest possible formula for the Skeleton View (diatomic hydrogen) and one for a fairly reasonable candidate for the Octets View (carbon tetrachloride).

--- ChemiaSwiftTests/ViewModel/SkeletonValidationTests.swift
+++ ChemiaSwiftTests/ViewModel/SkeletonValidationTests.swift
@@ -21,9 +21,25 @@ class SkeletonValidationTests: XCTestCase {
         super.tearDown()
     }
     
+    func testH2Complete() {
+        let centerAtom: Element = ElementFactory.create(withSymbol: .H)
+        let centerElementState: CenterElementState = CenterElementState(of: centerAtom)
+        centerElementState.suggestedElementSymbol = "H"
+        
+        var attachedElementStates: [AttachedElementState] = []
+        let nextAttachedState: AttachedElementState = AttachedElementState(of: ElementFactory.create(withSymbol: .H), withTarget: centerElementState)
+        nextAttachedState.setNumberOfBonds(to: 1)
+        nextAttachedState.suggestedElementSymbol = "H"
+        nextAttachedState.bondNumSuggested = 1
+        attachedElementStates.append(nextAttachedState)
+        
+        let formulaState: FormulaState = FormulaState(center: centerElementState, attached: attachedElementStates)
+        formulaState.checkCompletion(forRule: .skeleton)
+        
+        XCTAssertTrue(formulaState.isCompleted())
+    }
+    
     func testMethaneComplete() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
         let centerAtom: Element = ElementFactory.create(withSymbol: .C)
         let centerElementState: CenterElementState = CenterElementState(of: centerAtom)
         centerElementState.suggestedElementSymbol = "C"
@@ -41,6 +57,27 @@ class SkeletonValidationTests: XCTestCase {
         
         XCTAssertTrue(formulaState.isCompleted())
         
+    }
+    
+    func testCCl4RuleComplete() {
+        let centerAtom: Element = ElementFactory.create(withSymbol: .C)
+        let centerElementState: CenterElementState = CenterElementState(of: centerAtom)
+        centerElementState.suggestedElementSymbol = "C"
+        
+        var attachedElementStates: [AttachedElementState] = []
+        for _ in 1...4 {
+            let nextAttachedState: AttachedElementState = AttachedElementState(of: ElementFactory.create(withSymbol: .Cl), withTarget: centerElementState)
+            nextAttachedState.setNumberOfBonds(to: 1)
+            nextAttachedState.suggestedElementSymbol = "Cl"
+            nextAttachedState.bondNumSuggested = 1
+            attachedElementStates.append(nextAttachedState)
+        }
+        
+        let formulaState: FormulaState = FormulaState(center: centerElementState, attached: attachedElementStates)
+        formulaState.checkCompletion(forRule: .skeleton)
+        
+        XCTAssertFalse(formulaState.isCompleted())
+        XCTAssertTrue(formulaState.ruleCompleted())
     }
     
 //    func testPerformanceExample() {
